2586666622d16d5762a0b33b04673db03789e711
aybeedee <abdullahumer575@gmail.com>
2023-12-02 20:30:55 +0500

    add styling to home page

--- frontend/oauth-frontend/src/App.tsx
+++ frontend/oauth-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/oauth.png'
-import viteLogo from '/vite.svg'
+import OAuthSymbol from './assets/oauth.png'
+import GoogleIcon from './assets/google.png'
 import './App.css'
 
 function App() {
@@ -9,24 +9,25 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={OAuthSymbol} className="logo" alt="OAuth Symbol" />
       </div>
       <h1>OAuth Prototype</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          <div className="signin">
+            <img src={GoogleIcon} className="button-logo" />
+            <p>Sign in with Google</p>
+          </div>
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Click to sign in to the application with your Google account
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Prototype Implementation of OAuth
+      </p>
+      <p className="read-the-docs">
+        Supporting Google
       </p>
     </>
   )
