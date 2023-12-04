cb1c2104d80547f103c31fc769a64b4d308a6efe
aybeedee <abdullahumer575@gmail.com>
2023-12-04 15:13:41 +0500

    get google user via oauth

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import axios from 'axios'
+import qs from 'qs'
+import { createClient } from '@supabase/supabase-js'
+
 const app = express()
 const port = 3000
 
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://navarssqgacvvkcbemrx.supabase.co'
+dotenv.config()
+
+const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
@@ -11,23 +17,76 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post("/api/sessions/oauth/google", async (req, res)=> {
+app.get("/api/sessions/oauth/google", async (req, res) => {
 
+  // get the code from the query params
+  const code = req.query.code;
+
+  const url = "https://oauth2.googleapis.com/token";
   
-  const { data, error } = await supabase
-  .from('users')
-  .upsert({
-    email: 'someValue',
-    name: '',
-    picture: '',
-  })
-  .select()
-  if (error) {
-    return res.json(error)
+  const values = {
+    code,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    redirect_uri: process.env.GOOGLE_OAUTH_REDIRECT_URL,
+    grant_type: "authorization_code",
+  };
+
+  try {
+
+    // get the id and access token using the code
+    const tokenResult = await axios.post(
+      url,
+      qs.stringify(values),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    const { id_token, access_token } = tokenResult.data;
+
+    console.log("id_token: ", id_token);
+    console.log("access_token: ", access_token);
+
+    // get the user with the token
+    const userResult = await axios.get(
+      `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,
+      {
+        headers: {
+          Authorization: `Bearer ${id_token}`,
+        },
+      }
+    );
+
+    const user = userResult.data;
+
+    if (!user.verified_email) {
+      return res.status(403).send("The Google account's email is not verified");
+    }
+
+  } catch (error) {
+    return res.json(error);
   }
-  return res.json(data)
+
+
+
+  // const { data, error } = await supabase
+  // .from('users')
+  // .upsert({
+  //   email: 'someEmail',
+  //   name: 'someName',
+  //   picture: 'somePicture',
+  // })
+  // .select()
+  // if (error) {
+  //   return res.json(error)
+  // }
+  // return res.json(data)
+  res.send('Thanks for visiting this api route')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`OAuth backend app listening on port ${port}`)
 })
--- frontend/oauth-frontend/src/Home.tsx
+++ frontend/oauth-frontend/src/Home.tsx
@@ -2,8 +2,30 @@ import OAuthSymbol from './assets/oauth.png'
 import GoogleIcon from './assets/google.png'
 import './App.css'
 
-function Home() {
+function getGoogleOAuthURL() {
+  const rootURL = "https://accounts.google.com/o/oauth2/v2/auth";
+  const options = {
+    redirect_uri: import.meta.env.VITE_GOOGLE_OAUTH_REDIRECT_URL,
+    client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+    access_type: "offline",
+    response_type: "code",
+    prompt: "consent",
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ].join(" "),
+  };
 
+  console.log({ options });
+
+  const queryString = new URLSearchParams(options);
+  
+  console.log(queryString.toString());
+
+  return `${rootURL}?${queryString.toString()}`;
+}
+
+function Home() {
   return (
     <>
       <div>
@@ -11,12 +33,14 @@ function Home() {
       </div>
       <h1>OAuth Prototype</h1>
       <div className="card">
+        <a href={getGoogleOAuthURL()}>
         <button>
           <div className="signin">
             <img src={GoogleIcon} className="button-logo" />
             <p>Sign in with Google</p>
           </div>
         </button>
+        </a>
         <p>
           Click to sign in to the application with your Google account
         </p>
